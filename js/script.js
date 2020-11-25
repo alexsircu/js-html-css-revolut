fc18fd70f954fcf8dee6064c7334a7365420603b
Alex Sircu <alex.sricu@gmail.com>
2020-11-25 21:16:44 +0100

    Aggiunto js che fa comparire il dropdown

--- js/script.js
+++ js/script.js
@@ -1,1 +1,37 @@
-$(document).ready() 
+$(document).ready(
+  function () {
+    $("#subscription-container > a").mouseenter(
+      function () {
+        $(".subscription-dropdown").toggle();
+      }
+    );
+    $("#subscription-container > a").mouseleave(
+      function () {
+        $(".subscription-dropdown").toggle();
+      }
+    );
+
+    $("#who-we-are-container > a").mouseenter(
+      function () {
+        $(".who-we-are-dropdown").toggle();
+      }
+    );
+    $("#who-we-are-container > a").mouseleave(
+      function () {
+        $(".who-we-are-dropdown").toggle();
+      }
+    );
+
+    $("#help-container > a").mouseenter(
+      function () {
+        $(".help-dropdown").toggle();
+      }
+    );
+    $("#help-container > a").mouseleave(
+      function () {
+        $(".help-dropdown").toggle();
+      }
+    );
+
+  }
+);
